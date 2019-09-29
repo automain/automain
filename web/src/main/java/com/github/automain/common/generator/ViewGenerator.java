5f65233eee779ca98d599f8f871261fafb56f071
zhangyu <zhangyu13393@sina.com>
2019-09-29 23:58:28 +0800

    design upgrade

--- web/src/main/java/com/github/automain/common/generator/ViewGenerator.java
+++ web/src/main/java/com/github/automain/common/generator/ViewGenerator.java
@@ -13,9 +13,15 @@ public class ViewGenerator {
 
         boolean hasAdd = !addCheck.isEmpty();
         boolean hasSearch = !keyColumns.isEmpty();
+        boolean hasList = !listCheck.isEmpty();
+        boolean hasUpdate = !updateCheck.isEmpty();
+        boolean hasDetail = !detailCheck.isEmpty();
 
         if (hasAdd || hasSearch || hasIsValid) {
             resultStr += getBoxCard(columns, hasAdd, hasIsValid, tableName, keyColumns);
+        }
+        if (hasList) {
+            resultStr += getList(columns, hasIsValid, tableName, listCheck, dictionaryColumnList, sortCheck, hasUpdate,hasDetail);
         }
         return resultStr;
     }
@@ -51,5 +57,54 @@ public class ViewGenerator {
         return "\n        <el-card class=\"box-card\">\n            <el-form :inline=\"true\" :model=\"" +
                 lowerTableName + "VO\" size=\"mini\">\n" + addBlock + deleteBlock + searchBlock +
                 "            </el-form>\n        </el-card>";
+    }
+
+    private String getList(List<ColumnBean> columns, boolean hasIsValid, String tableName, List<String> listCheck, List<String> dictionaryColumnList, List<String> sortCheck, boolean hasUpdate, boolean hasDetail) {
+        StringBuilder listBlock = new StringBuilder();
+        String table = CommonGenerator.convertToJavaName(tableName, false);
+        for (ColumnBean column : columns) {
+            String columnName = column.getColumnName();
+            String lowerColumnName = CommonGenerator.convertToJavaName(columnName, false);
+            String columnComment = column.getColumnComment();
+            if (listCheck.contains(columnName)) {
+                if (CommonGenerator.checkTimeTypeColumn(column)) {
+                    listBlock.append("            <el-table-column prop=\"").append(lowerColumnName)
+                            .append("\" label=\"").append(columnComment)
+                            .append("\" width=\"160\" :formatter=\"dateTimeFormatter\"");
+                    if (sortCheck.contains(columnName)) {
+                        listBlock.append(" sortable=\"custom\"");
+                    }
+                    listBlock.append("></el-table-column>\n");
+                } else if (dictionaryColumnList.contains(columnName)) {
+                    listBlock.append("            <el-table-column prop=\"").append(lowerColumnName)
+                            .append("\" label=\"").append(columnComment)
+                            .append("\" column-key=\"").append(lowerColumnName)
+                            .append("\" :filters=\"").append(lowerColumnName)
+                            .append("Map\">\n                <template slot-scope=\"scope\">\n                    {{scope.row.")
+                            .append(lowerColumnName).append(" | dictionaryFilter(").append(lowerColumnName)
+                            .append("Key)}}\n                </template>\n            </el-table-column>\n");
+                } else {
+                    listBlock.append("            <el-table-column prop=\"").append(lowerColumnName)
+                            .append("\" label=\"").append(columnComment)
+                            .append("\" show-overflow-tooltip></el-table-column>\n");
+                }
+            }
+        }
+        StringBuilder operation = new StringBuilder();
+        if (hasUpdate || hasDetail) {
+            operation.append("            <el-table-column fixed=\"right\" label=\"操作\" width=\"100\">\n                <template slot-scope=\"scope\">\n");
+            if (hasUpdate) {
+                operation.append("                    <el-button @click=\"handleUpdate(scope.row)\" type=\"text\" size=\"small\">编辑</el-button>\n");
+            }
+            if (hasDetail) {
+                operation.append("                    <el-button @click=\"handleDetail(scope.row)\" type=\"text\" size=\"small\">详情</el-button>\n");
+            }
+            operation.append("                </template>\n            </el-table-column>\n");
+        }
+        String deleteSelect = hasIsValid ? "            <el-table-column type=\"selection\" width=\"42\" fixed=\"left\"></el-table-column>\n" : "";
+        return "\n        <el-table ref=\"multipleTable\" :data=\"pageBean.data\" tooltip-effect=\"dark\" :height=\"fullHeight\" @selection-change=\"selectToDelete\" @sort-change=\"handleSort\" @filter-change=\"handleFilterChange\">\n" +
+                deleteSelect + listBlock + operation +
+                "        </el-table>\n        <el-pagination @size-change=\"handleSizeChange\" @current-change=\"handlePageChange\" :page-sizes=\"[10, 20, 50, 100]\" :page-size=\"" +
+                table + "VO.size\" layout=\"->, total, prev, pager, next, jumper, sizes\" :total=\"pageBean.total\"></el-pagination>\n";
     }
 }
